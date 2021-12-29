d6174239fa8bfb98569a1a5b3ea0b47966685c83
Sergi Collado <sergi.ch8@gmail.com>
2021-12-29 20:30:52 +0100

    Added comment views

--- src/pages/Media/CommentAddView.vue
+++ src/pages/Media/CommentAddView.vue
@@ -1,13 +1,101 @@
 <template>
-
+  <q-dialog>
+    <q-card style="width: 600px">
+      <q-card-section>
+        <q-btn
+            round
+            flat
+            dense
+            icon="close"
+            class="float-right"
+            color="grey-8"
+            v-close-popup
+        ></q-btn>
+        <div class="text-h6">
+          Introduce your comment about {{ event.name }}
+        </div>
+      </q-card-section>
+      <q-separator inset></q-separator>
+      <q-card-section class="q-pt-none">
+        <q-form class="q-gutter-md full-width">
+          <q-item>
+            <div class="full-width" style="max-width: 500px">
+              <q-input v-model="text" clearable filled type="textarea"/>
+            </div>
+          </q-item>
+          <q-item v-if="user">
+            <q-checkbox v-model="anonymous" label="Publish as anonymous"/>
+          </q-item>
+        </q-form>
+      </q-card-section>
+      <q-card-section>
+        <q-card-actions align="right">
+          <q-btn
+              flat
+              label="CANCEL"
+              color="secondary"
+              dense
+              v-close-popup
+          ></q-btn>
+          <q-btn
+              flat
+              label="SEND"
+              color="primary"
+              dense
+              v-close-popup
+              @click="sendComment(event, text, anonymous)"
+          ></q-btn>
+        </q-card-actions>
+      </q-card-section>
+    </q-card
+    >
+  </q-dialog>
 </template>
-
 <script>
+import MediaService from "src/services/Media/media.service";
+import {computed, ref} from "vue";
+import {useQuasar} from "quasar";
+import {useStore} from "vuex";
+
 export default {
-  name: "CommentAddView.vue"
+  name: 'CommentAddView',
+  props: {
+    event: Object
+  },
+  setup() {
+    const text = ref("")
+    const anonymous = ref(false)
+    const user = computed(() => store.getters["user/getUser"])
+    const $q = useQuasar()
+    const store = useStore();
+
+    return{
+      user,
+      text,
+      anonymous,
+      sendComment: async (event, commentText, commentAnonymous) => {
+        if(commentText == ''){
+          $q.notify({ type: 'warning', message: "The comment can't be empty! Try again", color: 'secondary' })
+        } else {
+          const mediaService = new MediaService();
+          const comment = {
+            userId: commentAnonymous ? null : user.value.id,
+            eventId: event.id,
+            comment: commentText,
+            createdAt: Date.now(),
+          };
+          console.log(comment)
+          await mediaService.sendComment(comment);
+          await store.dispatch("media/addComment", comment);
+          text.value = ""
+          anonymous.value = null
+          $q.notify({ type: 'positive', message: 'Thanks! Your comment has been received correctly!', color: 'blue' })
+        }
+
+      },
+
+  }
+
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
--- src/pages/Media/CommentListView.vue
+++ src/pages/Media/CommentListView.vue
@@ -1,13 +1,58 @@
 <template>
+  <template v-if="comments">
+    <div class="row q-pa-md" style="max-width: 800px">
+      <q-list padding>
+        <q-item-label header class="text-h5 text-bold"
+        >Comments
+        </q-item-label
+        >
+        <template v-for="comment in comments" :key="comment.id">
+          <q-item class="wrap" v-ripple>
+            <q-item-section top avatar>
+              <q-avatar color="primary">
+                {{ getFirstLetter(comment.name) }}
+              </q-avatar
+              >
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>{{ comment.name }}</q-item-label>
+              <q-item-label caption :lines="2" class="wrap">{{ comment.comment }}</q-item-label>
+            </q-item-section>
+
+            <q-item-section side top>
+              <q-item-label caption
+              ><span v-if="comment.createdAt">{{formatDate(comment.createdAt)}}</span>
+                <span v-else>{{formatDate(Date.now())}}</span></q-item-label
+              >
+            </q-item-section>
+          </q-item>
+          <q-separator inset="item"/>
+        </template>
+      </q-list>
+    </div>
+  </template>
 
 </template>
-
 <script>
+import {date} from "quasar";
+
 export default {
-  name: "CommentListView.vue"
+  name: 'CommentListView',
+  props: {
+    comments: Array
+  },
+  setup(){
+    const formatDate = (dateToFormat) => {
+      return date.formatDate(dateToFormat, "D-MM-YYYY HH:mm");
+    };
+    const getFirstLetter = (name) => {
+      return name.slice(0, 1);
+    };
+    return{
+      formatDate,
+      getFirstLetter
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
