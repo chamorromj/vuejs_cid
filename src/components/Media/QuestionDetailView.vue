1fb7d2d144dc44cbf0f74273502e4bd37b3597f5
Sergi Collado <sergi.ch8@gmail.com>
2021-12-29 20:30:04 +0100

    Added question views

---
+++ src/components/Media/QuestionDetailView.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="q-pa-xs">
+    <div v-if="question.user" class="text-subtitle2">
+      {{ question.user.username }}
+    </div>
+    <q-field
+      color="orange"
+      filled
+      standout
+      label="Label"
+      stack-label
+      label-color="secondary"
+    >
+      <template v-slot:prepend>
+        <q-avatar color="primary">
+          {{ getFirstLetter("Anonymous") }}</q-avatar
+        >
+      </template>
+      <template v-slot:label>
+        <div class="row items-center all-pointer-events text-subtitle2 q-mb-sm">
+          <q-icon class="q-mr-xs" color="primary" size="sm" name="quiz" />
+          {{ question.title }} <span class="text-black q-ml-sm">({{formatDate(question.createdAt)}})</span>
+        </div>
+      </template>
+
+      <template v-slot:control>
+        <div class="self-center full-width no-outline" tabindex="0">
+           {{ question.message }}
+        </div>
+      </template>
+      <template
+        v-slot:append
+        v-if="(!question.response || question.response==1) && (isAdminUser || isSuperAdminUser)"
+      >
+        <q-icon
+          name="question_answer"
+          color="primary"
+          class="cursor-pointer"
+          @click="showAddResponse = true"
+          ><q-tooltip>Give an answer</q-tooltip></q-icon
+        >
+      </template>
+    </q-field>
+    <template v-if="question.response && question.response!= 1">
+      <ResponseDetailView :response="question.response" />
+    </template>
+  </div>
+
+  <ResponseAddView  v-model="showAddResponse" :question="question"/>
+</template>
+
+<script>
+import {ref} from "vue";
+import {useStore} from "vuex";
+import ResponseDetailView from "./ResponseDetailView.vue";
+import {date} from "quasar";
+import ResponseAddView from "components/Media/ResponseAddView";
+
+export default {
+  name: "DetailQuestionView",
+  components:{
+    ResponseAddView,
+    ResponseDetailView
+  },
+  props: {
+    question: Object,
+  },
+  setup() {
+    const showAddResponse = ref(false);
+    const store = useStore();
+
+
+
+    const getFirstLetter = (name) => {
+      return name.slice(0, 1);
+    };
+
+    const formatDate = (anyDate) => {
+      return date.formatDate(anyDate, "YYYY-MM-DD HH:mm");
+    };
+    return {
+      formatDate,
+      showAddResponse,
+      getFirstLetter,
+      isAdminUser: store.getters["user/isAdminUser"],
+      isSuperAdminUser: store.getters["user/isSuperAdminUser"],
+    };
+  },
+};
+</script>
+
--- src/components/Media/QuestionListView.vue
+++ src/components/Media/QuestionListView.vue
@@ -1,13 +1,76 @@
 <template>
+  <q-scroll-area style="height: 75vh">
+    <div
+      v-if="questions && questions.length == 0"
+      class="row text-subtitle1 text-bold q-px-md"
+    >
+      This event has no questions
+    </div>
+    <div v-else>
+      <div
+        class="q-py-xs column q-mb-xs q-px-md"
+        v-for="question in questions"
+        :key="question.id"
+      >
+        <QuestionDetailView :question="question" />
+      </div>
+    </div>
+    <q-icon
+      v-if="!showAddQuestion"
+      class="q-ma-md cursor-pointer"
+      name="add_circle"
+      color="primary"
+      size="xl"
+      @click="addQuestion"
+    >
+      <q-tooltip>New question</q-tooltip>
+    </q-icon>
 
+    <QuestionAddView v-model="showAddQuestion"/>
+  </q-scroll-area>
 </template>
 
 <script>
+import {computed, defineAsyncComponent, onBeforeMount, ref} from "vue";
+import {useStore} from "vuex";
+import QuestionAddView from "components/Media/QuestionAddView";
+
 export default {
-  name: "QuestionListView.vue"
-}
+  name: "QuestionListView",
+  components: {
+    QuestionAddView,
+    QuestionDetailView: defineAsyncComponent(() =>
+      import("./QuestionDetailView.vue")
+    ),
+  },
+  props:{
+    event:{
+      type: Object
+    }
+  },
+
+  setup(props, context) {
+    const showAddQuestion = ref(false);
+    const store = useStore();
+    const user = computed(() => store.getters["user/getUser"]);
+    const event = computed(() => store.getters["event/getEvent"]);
+    const questions = computed(() => store.getters["media/getQuestions"]);
+
+
+    onBeforeMount(async()=>{
+      await store.dispatch("media/addQuestions", props.event.id);
+
+    })
+
+
+    return {
+      showQuestionForm: ref(false),
+      addQuestion: () => (showAddQuestion.value = true),
+      questions,
+      showAddQuestion,
+      user,
+    };
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
