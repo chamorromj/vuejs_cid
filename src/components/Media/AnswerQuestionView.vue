fe3daf477b3c14034fd75c304bc82beacbaf6f96
Sergi Collado <sergi.ch8@gmail.com>
2021-12-29 20:32:14 +0100

    Added answer and response views

--- src/components/Media/AnswerQuestionView.vue
+++ src/components/Media/AnswerQuestionView.vue
@@ -1,13 +1,57 @@
 <template>
+  <div class="row q-pa-md justify-center">
 
+    {{question}}
+    <q-form
+      @submit.prevent="onSubmit"
+      class="q-gutter-md col-xs-12 col-md-8 q-py-md"
+      autofocus
+    >
+      <q-input filled type="text" v-model="message" label="Answer" />
+
+      <div class="row justify-right">
+        <q-btn label="Submit" type="submit" class="q-mr-md" color="primary" />
+        <q-btn label="Cancel" type="button" color="secondary" />
+      </div>
+    </q-form>
+  </div>
 </template>
-
 <script>
+import { ref } from "vue";
+import {useStore} from "vuex"
+import {useQuasar} from "quasar";
 export default {
-  name: "AnswerQuestionView.vue"
-}
+  name: "AnswerQuestionView",
+  props: {
+    question: Object,
+  },
+
+  setup(props) {
+    const message = ref(null);
+    const store = useStore();
+    const response = ref(null)
+    const $q = useQuasar()
+
+    const onSubmit = async () => {
+      if(message.value==''){
+        $q.notify({ type: 'warning', message: "The response can't be empty! Try again", color: 'secondary' })
+      } else{
+        response.value = {
+          questionId: props.question.id,
+          message: message.value,
+        };
+        console.log(response.value);
+        await store.commit("media/popQuestion", props.question.id);
+      }
+
+
+    };
+
+    return {
+      message,
+      onSubmit,
+      response
+    };
+  },
+};
 </script>
-
-<style scoped>
-
-</style>
--- src/components/Media/ResponseAddView.vue
+++ src/components/Media/ResponseAddView.vue
@@ -1,13 +1,97 @@
 <template>
-
+  <q-dialog>
+    <q-card style="width: 600px">
+      <q-card-section>
+        <q-btn
+            round
+            flat
+            dense
+            icon="close"
+            class="float-right"
+            color="grey-8"
+            v-close-popup
+        ></q-btn>
+        <div class="text-h6">Introduce your response</div>
+      </q-card-section>
+      <q-separator inset></q-separator>
+      <q-card-section class="q-pt-none">
+        <q-form class="q-gutter-md">
+          <q-list>
+            <q-item>
+              <q-item-section>
+                <q-item-label class="q-pb-xs">Message</q-item-label>
+                <q-input
+                    filled
+                    type="text"
+                    v-model="message"
+                    label="Response"
+                />
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-form>
+      </q-card-section>
+      <q-card-section>
+        <q-card-actions align="right">
+          <q-btn
+              flat
+              label="CANCEL"
+              color="secondary"
+              dense
+              v-close-popup
+          ></q-btn>
+          <q-btn
+              flat
+              label="SEND"
+              color="primary"
+              dense
+              v-close-popup
+              @click="addResponse"
+          >
+          </q-btn>
+        </q-card-actions>
+      </q-card-section>
+    </q-card
+    >
+  </q-dialog>
 </template>
-
 <script>
+import {useStore} from "vuex";
+import {ref} from "vue";
+import {useQuasar} from "quasar";
+import {useRoute} from "vue-router";
+
 export default {
-  name: "ResponseAddView.vue"
+  name: 'ResponseAddView',
+  props:{
+    question: Object
+  },
+  setup(props){
+    const store = useStore()
+    const $q = useQuasar()
+    const message = ref('')
+    const route = useRoute()
+
+    return{
+      message,
+      addResponse: async () => {
+        if(props.question.id){
+          const response = {
+            message: message.value,
+            questionId: props.question.id,
+            createdAt: Date.now(),
+          };
+          if(route.name.includes("question")){
+            await store.commit("event/popQuestion", props.question.id);
+          }
+          const ok = await store.dispatch("media/addResponse", response);
+          if(ok){
+            $q.notify({ type: 'positive', message: 'The response has been registered', color: 'blue', icon: 'thumb_up' })
+          }
+        }
+
+      }
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
